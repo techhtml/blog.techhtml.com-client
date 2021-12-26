265a786c862759bb5b6e3000500046327c45c8e6
techhtml <apes0123@gmail.com>
2021-12-26 20:47:13 +0900

    ✨ POST 추가

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,5 +1,20 @@
+import axios from "axios";
 import { NextPage } from "next";
-import { useState } from "react";
+import React, { useState } from "react";
+
+const write = (title: string, post: string) => {
+  axios.post('http://localhost:1337/api/posts/', {
+    "data": {
+      "author": "조은",
+      "title": title,
+      "contents": post,
+    }
+  }).then((res) => {
+    if(res.status === 200) {
+      location.href = '/posts'
+    }
+  })
+}
 
 const WritePage:NextPage = () => {
   const [title, setTitle] = useState('');
@@ -13,10 +28,15 @@ const WritePage:NextPage = () => {
     setTitle(e.target.value);
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    write(title, post);
+  }
+
   return (
     <div>
       <h1>글 작성</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleTitleChange} value={title} />
         <textarea onChange={handleTextAreaChange} value={post} />
         <button type="submit">발행하기</button>
